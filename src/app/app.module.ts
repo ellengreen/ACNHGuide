abab48b642cd23a798e159e5cde38b358986727a
Ellen <eegreen.95@gmail.com>
2020-05-25 01:24:34 -0500

    added firebase to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -9,6 +13,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BugsComponent } from './critterpedia/bugs/bugs.component';
 import { FishComponent } from './critterpedia/fish/fish.component';
 import { CritterpediaMainComponent } from './critterpedia/critterpedia-main/critterpedia-main.component';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +26,9 @@ import { CritterpediaMainComponent } from './critterpedia/critterpedia-main/crit
     CritterpediaMainComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule,
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAQduFd1AHmB0YP_3Fp-ubpjHm38EGuyrs",
+    authDomain: "animal-crossing-92e14.firebaseapp.com",
+    databaseURL: "https://animal-crossing-92e14.firebaseio.com",
+    projectId: "animal-crossing-92e14",
+    storageBucket: "animal-crossing-92e14.appspot.com",
+    messagingSenderId: "51835103867",
+    appId: "1:51835103867:web:7b725930f55abc68da50de",
+    measurementId: "G-GVWSVJZQHY"
+  }
 };
 
 /*
