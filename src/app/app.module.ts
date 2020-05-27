e1ef18482b8287dba94dc7a203b4964c5462d369
Ellen <eegreen.95@gmail.com>
2020-05-27 17:03:21 -0500

    removed files unncessary for this branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { BugsComponent } from './critterpedia/bugs/bugs.component';
 import { FishComponent } from './critterpedia/fish/fish.component';
 import { CritterpediaMainComponent } from './critterpedia/critterpedia-main/critterpedia-main.component';
@@ -14,9 +11,6 @@ import { CritterpediaMainComponent } from './critterpedia/critterpedia-main/crit
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    ProfileComponent,
-    DashboardComponent,
     BugsComponent,
     FishComponent,
     CritterpediaMainComponent
