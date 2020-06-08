6b198e0d43cf26c882ead873e0bdcdc1e4b98b95
Ellen <eegreen.95@gmail.com>
2020-06-07 19:19:02 -0500

    created TS function for finding villager birthday rather than in html file

--- src/app/dashboard/birthday/birthday.component.ts
+++ src/app/dashboard/birthday/birthday.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrentDateService } from 'src/app/shared/current-date.service';
 import { NookipediaService } from 'src/app/shared/nookipedia.service';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-birthday',
@@ -11,17 +12,35 @@ export class BirthdayComponent implements OnInit {
 
   constructor(
     public nookService: NookipediaService,
-    public dateService: CurrentDateService
+    public dateService: CurrentDateService,
+    public kvPipe: KeyValuePipe
   ) { }
 
   bdayFormat = this.dateService.bdayFormat;
   villagers: any;
-
+  bday: any;
+  bdayID: number; 
   ngOnInit() {
     this.nookService.getVillagers().subscribe(data=> {
       this.villagers = data;
+      this.kvPipe.transform(this.villagers);
+      this.birthday();
     });
   }
+
+  birthday(){
+    Object.keys(this.villagers).forEach(key => {
+      if (this.villagers[key]['birthday']==this.bdayFormat){
+        this.bday = this.villagers[key]['name']['name-en'];
+        this.bdayID = this.villagers[key]['id'];
+        console.log(this.bday)
+      }
+    })
+  }
+
+
+
+
 }
 
 
