3dcb339e9699e3b003d16b9f79932211f23c370e
Ellen <eegreen.95@gmail.com>
2020-06-11 15:12:16 -0500

    navbar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor(
-    ) { }
+  constructor(public authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
