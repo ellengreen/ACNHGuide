763ca041772a1130064c45a66ba9544a4d5636de
Ellen <eegreen.95@gmail.com>
2020-06-21 21:56:34 -0500

    progress bars for collection

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/shared/firebase.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: FirebaseService) { }
 
+  savedFish:any;
+  savedBugs: any;
   ngOnInit(): void {
+    this.db.fetchFish().subscribe(fish =>{
+      this.savedFish=fish;
+      this.fishWidth = (this.savedFish.length/80)*100;
+    })
+    this.db.fetchBugs().subscribe(bugs=> {
+      this.savedBugs=bugs;
+      this.bugWidth = (this.savedBugs.length/80)*100;
+    })
   }
 
+  
   displayName: string;
   addName(name: string){
     this.displayName = name;
   }
+
+  fishWidth: number;
+  bugWidth: number;
+
 
 }
