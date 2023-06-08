5d782f5ea5b6f7056c99ffea9971bf6a091ef94d
Ellen Green <eegreen.95@gmail.com>
2023-06-07 23:11:45 -0500

    V1: more css cleanup

--- src/app/components/critterpedia/critterpedia-table.component.ts
+++ src/app/components/critterpedia/critterpedia-table.component.ts
@@ -1,8 +1,6 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { CritterType } from 'app/shared/enums/critter-type.enum';
 import { Critter } from 'app/shared/interfaces/critter';
-import { CritterInfoDialogComponent } from '../critter-info-dialog/critter-info-dialog.component';
 
 @Component({
   selector: 'app-critterpedia-table',
@@ -13,35 +11,20 @@ export class CritterpediaTableComponent implements OnChanges {
 
   @Input() critterpediaMode: CritterType = CritterType.fish;
   @Input() critterList: Critter[];
-  critterType = CritterType;
 
+  
   @Output() caughtClicked: EventEmitter<any> = new EventEmitter<any>();
   @Output() removeClicked: EventEmitter<any> = new EventEmitter<any>();
-
+  
+  critterType = CritterType;
   selectedCritter: Critter;
 
-  constructor(private dialog: MatDialog) { }
+  constructor() { }
 
   ngOnChanges(): void {
     if (this.critterList) {
       this.selectedCritter = this.critterList[0];
     }
-  }
-
-  openCritterInfoDialog(critter: Critter) {
-    this.dialog.open(CritterInfoDialogComponent, {
-      data: {
-        selectedCritter: critter,
-        critterpediaMode: this.critterpediaMode
-      },
-      panelClass: 'critter-dialog',
-      // maxHeight: '450px',
-      minWidth: '600px',
-    }).componentInstance.caughtClicked.subscribe(selectedCritter => {
-      this.caughtClicked.emit(selectedCritter);
-    })
-
-    // Will need to use dialog ref, for deleting etc
   }
 
   selectCritter(critter: Critter): void {
